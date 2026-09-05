0019472b6f11c3a0b18afc3b498ef7d9d6d77887
HyMe code <hymecode@gmail.com>
2026-09-05 22:13:00 +0500

    updated

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,10 +3,14 @@ from aiogram import Dispatcher
 def setup_handlers(dp: Dispatcher):
     from .start import router as start_router
     from .translate import router as translate_router
-    from .premium import router as premium_router
-    from .promo import router as promo_router  # YANGI
+    # from .pomodoro import router as pomodoro_router   # keyin
+    # from .premium import router as premium_router     # keyin
+    # from .promo import router as promo_router         # keyin
+    # from .profile import router as profile_router     # keyin
 
     dp.include_router(start_router)
     dp.include_router(translate_router)
-    dp.include_router(premium_router)
-    dp.include_router(promo_router)  # YANGI
+    # dp.include_router(pomodoro_router)   # keyin
+    # dp.include_router(premium_router)    # keyin
+    # dp.include_router(promo_router)      # keyin
+    # dp.include_router(profile_router)    # keyin
