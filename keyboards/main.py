ab3f15db2e968e1f592fc34bdad4001e3b503d40
HyMe code <hymecode@gmail.com>
2026-09-06 01:12:54 +0500

    Update main.py

--- keyboards/main.py
+++ keyboards/main.py
@@ -6,7 +6,7 @@ def get_main_keyboard():
             [KeyboardButton(text="🌍 Tarjima")],
             [KeyboardButton(text="🍅 Pomodoro"), KeyboardButton(text="⭐ Premium")],
             [KeyboardButton(text="🎲 Randoms"), KeyboardButton(text="🎁 Promo-kod")],
-            [KeyboardButton(text="👤 Profil")]
+            [KeyboardButton(text="👤 Profil")],
             [KeyboardButton(text="🆘 Support")]
         ],
         resize_keyboard=True,
