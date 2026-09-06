41a4dbffc87968573b1daed5153f15182d39166b
HyMe code <hymecode@gmail.com>
2026-09-07 02:16:08 +0500

    Update test_ids.py

--- test_ids.py
+++ test_ids.py
@@ -3,13 +3,11 @@
 # IELTS testlari ro'yxati
 # Har bir test: {"chat_id": guruh_id, "message_id": xabar_id}
 IELTS_TESTS = [
-    {"chat_id": -1003863295329, "message_id": 100},  # 1-test
-    {"chat_id": -1003863295329, "message_id": 101},  # 2-test
     # ... 10 tagacha yoki ko'proq
 ]
 
 # SAT testlari ro'yxati
 SAT_TESTS = [
-    {"chat_id": -1003863295329, "message_id": 'AgADZ6YAAji88Ug'},  # 1-test
+    {"chat_id": -1003863295329, "message_id": "AgADZ6YAAji88Ug"},  # 1-test
     # ... 10 tagacha yoki ko'proq
 ]
