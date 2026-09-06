1bce8c79e25f0dbe7a31775306a58946fbc98b75
HyMe code <hymecode@gmail.com>
2026-09-06 12:33:24 +0500

    Update main.py

--- keyboards/main.py
+++ keyboards/main.py
@@ -3,12 +3,11 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 def get_main_keyboard():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🌍 Tarjima")],
+            [KeyboardButton(text="🌍 Tarjima"), KeyboardButton(text="🎲 Randoms")],
             [KeyboardButton(text="🍅 Pomodoro"), KeyboardButton(text="⭐ Premium")],
-            [KeyboardButton(text="🎲 Randoms"), KeyboardButton(text="🎁 Promo-kod")],
-            [KeyboardButton(text="👤 Profil")],
+            [KeyboardButton(text="🎁 Promo-kod"), KeyboardButton(text="👤 Profil")],
             [KeyboardButton(text="🆘 Support")]
         ],
         resize_keyboard=True,
-        one_time_keyboard=False
+        one_time_keyboard=False 
     )
