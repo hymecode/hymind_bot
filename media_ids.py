53acd1bcb6360bb7a47806fad3e11a2e43e5a58d
HyMe code <hymecode@gmail.com>
2026-09-08 23:24:53 +0500

    added new uzb movies

--- media_ids.py
+++ media_ids.py
@@ -8,23 +8,45 @@
 UZB_MEDIA = {
     "movies": [
         # Oddiy film (qismsiz)
-        {"chat_id": -1003863295329, "message_id": 104, "title": "Film nomi 1"},
+        {"chat_id": -1003863295329, "message_id": 1, "title": "bosma! film yoq baribir"},
         
         # Qismlari bo'lgan film (Marvel guruhi)
         {
-            "title": "Marvel",
+            "title": "Temir Odam [1,2,3]",
             "episodes": [
-                {"chat_id": -1003863295329, "message_id": 102, "title": "Temir Odam (2008)"},
-                {"chat_id": -1003863295329, "message_id": 104, "title": "Temir Odam (2011)"},
+                {"chat_id": -1003863295329, "message_id": 102, "title": "[1] Temir Odam 1 (2008)"},
+                {"chat_id": -1003863295329, "message_id": 104, "title": "[2] Temir Odam 2 (2010)"},
+                {"chat_id": -1003863295329, "message_id": 106, "title": "[3] Temir Odam 3 (2013)"}
             ]
-        },
+        }
         
         # Yana yangi film qo'shish uchun shu yerda davom eting (masalan Spiderman)
+    "title": "O'rgimchak odam [1,2,3,4]",
         {
-            "title": "Spiderman",
             "episodes": [
-                {"chat_id": -1003863295329, "message_id": 200, "title": "Spiderman 1"},
-                {"chat_id": -1003863295329, "message_id": 201, "title": "Spiderman 2"}
+                {"chat_id": -1003863295329, "message_id": 108, "title": "[1] Uyga qaytish (2017)"},
+                {"chat_id": -1003863295329, "message_id": 110, "title": "[2] Uydan uzoqda (2019)"},
+                {"chat_id": -1003863295329, "message_id": 112, "title": "[3] Uyga yo'l yo'q (2021)"},
+                {"chat_id": -1003863295329, "message_id": 114, "title": "[4] Yangi kun (2026)"}
+            ]
+        }
+        {
+            "title": "Men Grutman [1,2,3,4,5]",
+            "episodes": [
+                {"chat_id": -1003863295329, "message_id": 116, "title": "Men Grutman 1"},
+                {"chat_id": -1003863295329, "message_id": 118, "title": "Men Grutman 2"},
+                {"chat_id": -1003863295329, "message_id": 120, "title": "Men Grutman 3"},
+                {"chat_id": -1003863295329, "message_id": 122, "title": "Men Grutman 4"},
+                {"chat_id": -1003863295329, "message_id": 124, "title": "Men Grutman 5"},
+            ]
+        },
+        {
+            "title": "Kapitan Amerika [1,2,3,4,5]",
+            "episodes": [
+                {"chat_id": -1003863295329, "message_id": 116, "title": "[1] Birinchi Qasoskor (2011)"},
+                {"chat_id": -1003863295329, "message_id": 118, "title": "[2] Qishki Askar (2014)"},
+                {"chat_id": -1003863295329, "message_id": 120, "title": "[3] Fuqarolik Urushi (2016)"},
+                {"chat_id": -1003863295329, "message_id": 122, "title": "[4] Jasur Yangi Dunyo (2025)"},
             ]
         },
     ],
@@ -43,6 +65,13 @@ UZB_MEDIA = {
         },
     ],
 }
+
+
+
+
+
+
+
 
 ENG_MEDIA = {
     "movies": [
